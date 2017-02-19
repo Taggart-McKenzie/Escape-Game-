ac112cc14a4ada2486e2cca448f069063bda317a
samanthabrown <samanthabrown@localhost>
2017-02-18 21:45:36 -0800

    another step closer

--- Escape-Game-/src/byui/cit260/escapeGame/view/MainMenuView.java
+++ Escape-Game-/src/byui/cit260/escapeGame/view/MainMenuView.java
@@ -17,20 +17,20 @@ public class MainMenuView {
    
    public MainMenuView() {
        this.menu= "\n"
-               +"\n------------------------------------"
-                     +"\n| Welcome to the Main Menu   |"
-                     +"\n ----------------------------"
+                     +"\n------------------------------------------------------"
+                     +"\n| Welcome to the Main Menu Choose from the following  |"
+                     +"\n -----------------------------------------------------"
                      +"\nS - Save"
                      +"\nB - Begin New Game"
                      +"\nL - Load Saved Game"
                      +"\nH - Help"
                      +"\nQ - Quit"
-                     +"\n ----------------------------";
+                     +"\n -----------------------------------------------------";
    }
   
    public void displayMainMenuView() {
    
-       boolean done = false;
+       boolean done = false;// set flag to not done 
        do{
            //prompt for and get players name
        String menuOption = this.getMenuOption();
@@ -46,30 +46,62 @@ public class MainMenuView {
 }
 
 private String getMenuOption() {
-    
-   Scanner keyboard = new Scanner(System.in); //get infile for keyboard
-        String value = ""; //value to be returned
-        boolean valid = false; //initialize to not valid
-        
-        while (!valid) {//loop while an invalid value is enter
-            System.out.println("\n" + this.menu);
-            
-            value = keyboard.nextLine(); //get next line typed on keyboard
-            value = value.trim(); //trim off leading and trailing blanks
-            
-            if (value.length() < 1){ //value is blank
-                System.out.println("\nInvalid value: value cannot be blank");
-                continue;
-            }
-            
-            break; //end the loop
-} return value;
+        System.out.println("\n*** getMenuOption() funciton called***");
+            return "N";
 }
-private boolean doAction(String menuOption) {
-    System.out.println("\n*** doAction() function called ***)");
-    return true;
+            
+private boolean doAction(String menuOption){
+   
+    menuOption = menuOption.toUpperCase();
+    
+    switch(menuOption){
+        case "S":
+            this.saveGame();
+            break;
+        case "B":
+            this.beginNewGame();
+            break;
+        case "L":
+            this.loadSavedGame();
+            break;
+        case "H":
+            this.help();
+            break;
+        case "Q":
+            this.quit();
+            break;
+        default:
+            System.out.println("\n*** Invalit selection Please Try Again.***");
+            break;
+    } 
+        return false;
 }
 
-    
+
+        
+        private void saveGame(){
+            System.out.println("*** startSaveGame funtion called***");
+        }
+        
+        private void beginNewGame(){
+            System.out.println("*** startBeginNewGame function called***");
+        }
+        
+        private void loadSavedGame(){
+            System.out.println("*** startLoadSavedGame function called*** ");
+        }
+        private void help(){
+            System.out.println("*** startHelp function called***");
+        }
+        
+        private void quit(){
+            System.out.println("*** startQuit function called***");
+        }
 }
+        
+
+
+
+    
+
 
