81ada3851c837d4f3c8dae5400de34b78be9f440
Daniel <Daniel@192.168.1.144>
2017-02-22 18:34:10 -0700

    formatted some really ugly code

--- Escape-Game-/src/byui/cit260/escapeGame/control/ActionControl.java
+++ Escape-Game-/src/byui/cit260/escapeGame/control/ActionControl.java
@@ -10,121 +10,120 @@ package byui.cit260.escapeGame.control;
  * @author samanthabrown
  */
 public class ActionControl {
-   public int moveUp(int location){
-       int x = 0;
-       int nextLocation;
-       nextLocation = location + x;
-       return nextLocation;
-   } 
-   public int moveDown (int location){
-       int x = 0;
-       int nextLocation;
-       nextLocation = location + x;
-       return nextLocation;
-   }
-    public int moveLeft (int location){
-       int x = 0;
-       int nextLocation;
-       nextLocation = location + x;
-       return nextLocation;
-   }
-    public int moveRight (int location){
-       int x = 0;
-       int nextLocation;
-       nextLocation = location + x;
-       return nextLocation;
-   }
-   public int goBack(int location){
-       int prevLocation = location--;
-       return prevLocation;
-   }
-   public double pickUpItem(double problem, int item){
-       double solveProblem = 0;   
-       return item;
-   }
-   
-   public String useItem(String item){
-       String clue = item;
-       return clue;
-   }
-   public double solveTriangle(double base, double height){
-       base = 0;
-       height = 0;
-       double surfaceArea = 0;
-       
-       if (base < 0 || base > 10){
-           return -1;
-       }
-           else if (height < 0 || height > 10){
-                   return -1;
-       }
-           else if ((base >= 0 && base < 11) && (height >= 0 && height <11)){
-                   surfaceArea = (base*height)/2;
-                   return surfaceArea;
-       }
-       return surfaceArea;
-   }
-   public double solveForX(double y, double z){
-      double x = 0.0;
-       
-      if (y<0){
-          return -1;
-      } 
-      else if(z < 0){
-          return -1;
-      }
-      else if(z>y){
-          return -1;
-      }
-      else if(y>0 && x>0 && z<y){
-          y = 50;
-          z = 10;
-          x = (y-z)/5;
-          return x;
-      }
-      return x;
-   } 
-   public double solveCube(double base, double height, double length){
-       double volume = 0.0;
-       
-       	
-                if(base<0 || base>10){
+
+    public int moveUp(int location) {
+        int x = 0;
+        int nextLocation;
+        nextLocation = location + x;
+        return nextLocation;
+    }
+
+    public int moveDown(int location) {
+        int x = 0;
+        int nextLocation;
+        nextLocation = location + x;
+        return nextLocation;
+    }
+
+    public int moveLeft(int location) {
+        int x = 0;
+        int nextLocation;
+        nextLocation = location + x;
+        return nextLocation;
+    }
+
+    public int moveRight(int location) {
+        int x = 0;
+        int nextLocation;
+        nextLocation = location + x;
+        return nextLocation;
+    }
+
+    public int goBack(int location) {
+        int prevLocation = location--;
+        return prevLocation;
+    }
+
+    public double pickUpItem(double problem, int item) {
+        double solveProblem = 0;
+        return item;
+    }
+
+    public String useItem(String item) {
+        String clue = item;
+        return clue;
+    }
+
+    public double solveTriangle(double base, double height) {
+        base = 0;
+        height = 0;
+        double surfaceArea = 0;
+
+        if (base < 0 || base > 10) {
             return -1;
-   }
-        else if(height<0 || height>10){
-		return -1;
-   }
-        else if(length<0 || length>10){
-		return -1;
-   }
-        else if((base >= 0 && base<11) && (height >=0 && height<11)
-                 && (length >=0 && length<11)){
- 	volume = base*height*length;
+        } else if (height < 0 || height > 10) {
+            return -1;
+        } else if ((base >= 0 && base < 11) && (height >= 0 && height < 11)) {
+            surfaceArea = (base * height) / 2;
+            return surfaceArea;
+        }
+        return surfaceArea;
+    }
+
+    public double solveForX(double y, double z) {
+        double x = 0.0;
+
+        if (y < 0) {
+            return -1;
+        } else if (z < 0) {
+            return -1;
+        } else if (z > y) {
+            return -1;
+        } else if (y > 0 && x > 0 && z < y) {
+            y = 50;
+            z = 10;
+            x = (y - z) / 5;
+            return x;
+        }
+        return x;
+    }
+
+    public double solveCube(double base, double height, double length) {
+        double volume = 0.0;
+
+        if (base < 0 || base > 10) {
+            return -1;
+        } else if (height < 0 || height > 10) {
+            return -1;
+        } else if (length < 0 || length > 10) {
+            return -1;
+        } else if ((base >= 0 && base < 11) && (height >= 0 && height < 11)
+                && (length >= 0 && length < 11)) {
+            volume = base * height * length;
+            return volume;
+        }
         return volume;
-   }
-       return volume;
-   }
-   public double solvePower(double x){
-       double power = 0;
-       
-       if(x<1 || x > 10){
-           return -1;
-       }
-       else if (x>=1 && x<=10){
-           power = x*x;
-           return power;
-       }
-       return power;
-   } 
-   
-   public boolean quit(boolean answer){
+    }
+
+    public double solvePower(double x) {
+        double power = 0;
+
+        if (x < 1 || x > 10) {
+            return -1;
+        } else if (x >= 1 && x <= 10) {
+            power = x * x;
+            return power;
+        }
+        return power;
+    }
+
+    public boolean quit(boolean answer) {
         boolean location = true;
         boolean GameControl = false;
-        
-        if (answer==true){
+
+        if (answer == true) {
             return location;
-        }
-        else {
+        } else {
             return GameControl;
         }
     }
